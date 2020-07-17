c5d5bb492b8cbd438d3e1feb50bbb60a8009fe2f
Dan K <KC135Q@users.noreply.github.com>
2020-07-17 09:33:10 -0400

    Decent start on pseudocode

--- guess.js
+++ guess.js
@@ -1,1 +1,17 @@
 console.log("Welcome to the guessing game")
+
+// Define variables: wins, word to guess, wrong guesses, guesses left, wins and losses
+
+// Define functions: start game, display the word (formatting), get word to guess, update display
+// Define event listeners: listen for a key press,
+
+function wordStuff() {
+  // Word Entry:
+  // If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+  // As the user guesses the correct letters, reveal them: m a d o _ _ a.
+  // Number of Guesses Remaining: # of guesses remaining for the user.
+  // Letters Already Guessed: Shows the hetters a user has already guessed, displayed like: L, Z, Y, H.
+  // After the user wins or loses, the game should automatically choose another word from a pre-set list and revert back to the start screen: "Press any key to get started!"
+}
+
+// Call functions, do logic
